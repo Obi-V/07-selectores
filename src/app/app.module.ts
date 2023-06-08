cca6ff688e9575e2b54a716c5e4af8c9a2da9735
Obi-V <skadring@gmail.com>
2023-06-08 14:53:18 +0200

    Update Read.me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
-
-
-
 @NgModule({
   declarations: [
     AppComponent
